f5e1df28a7b1bbfcb0a247f18dca9203b7eb5af6
Kostrula <d.galkin2011@yandex.ru>
2021-01-20 19:51:55 +0300

    First commit, Added moving Hero, added worm, fly, bomber, slime, barnacle, added Tilemap

---
+++ Assets/Scripts/Beetle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Beetle : MonoBehaviour
+{
+    public float speed = 4f;
+    bool isHidden = false;
+    bool isWait = false;
+    public float waitTime = 4f;
+    public Transform point;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        point.transform.position = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (!isWait)
+       {
+            transform.position = Vector3.MoveTowards
+            (transform.position, 
+            point.position, speed * Time.deltaTime);
+
+       }
+    }
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<Player>().RecountHp(-1);
+            collision.gameObject.GetComponent<Rigidbody2D>().
+            AddForce(transform.up * 
+            collision.gameObject.GetComponent<Player>().jumpHeight, 
+            ForceMode2D.Impulse);
+        }
+    }
+
+    
+}
